7cd8f2d8ec99aff733b76051e61eb7e5c0369748
Darren McLean <deemclean46@gmail.com>
2022-02-07 11:00:20 +0000

    New Table to DB added

---
+++ Server/Data/Migrations/20220207105756_updatedCars11.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Data.Migrations
+{
+    public partial class updatedCars11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
