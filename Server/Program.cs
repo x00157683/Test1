facc4ddfc6636aeb7bd7621b7c4b21b027faf9f4
Darren McLean <deemclean46@gmail.com>
2022-02-09 11:55:50 +0000

    Adjusting the server pipeline

--- Server/Program.cs
+++ Server/Program.cs
@@ -28,9 +28,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
+{
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "WarpServer API");
+    swaggerUIOptions.RoutePrefix = string.Empty;
+        
+});
 
 app.UseHttpsRedirection();
 
